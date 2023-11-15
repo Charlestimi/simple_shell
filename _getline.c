f67e36e11bf7a6e5c414e286901a5ea33868250f
ZobaOkafor <judithokafor30@gmail.com>
2023-11-15 10:35:54 -0800

    Updated simple shell

--- _getline.c
+++ _getline.c
@@ -20,35 +20,106 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	(void)stream;
 	if (lineptr == NULL || n == NULL)
 		return (-1);
+
 	if (*lineptr == NULL || *n == 0)
-	{	*n = BUFFER_SIZE;
-		*lineptr = malloc(*n);
-		if (*lineptr == NULL)
-		{	perror("Memory allocation failed");
-			exit(EXIT_FAILURE); } }
+		init_lineptr(lineptr, n);
+
 	for (;;)
 	{
 		if (idx == B_read)
-		{	B_read = read(STDIN_FILENO, buff, BUFFER_SIZE);
-			if (B_read <= 0)
-			{
-				if (num == 0)
-					return (-1); }
-			idx = 0; }
+			fill_buff(buff, &idx, &B_read);
+
 		while (idx < B_read)
 		{
 			c = buff[idx++];
-			if (c == '\n')
-			{	(*lineptr)[num] = '\0';
-				return (num); }
-			if (num + 1 >= *n)
-			{	*n += BUFFER_SIZE;
-				*lineptr = realloc(*lineptr, *n);
-				if (*lineptr == NULL)
-				{
-					perror("Memory reallocation failed");
-					exit(EXIT_FAILURE); } }
-			(*lineptr)[num++] = c; } }
+			if (handle_char(c, lineptr, n, &num))
+				return (num);
+		}
+	}
 	return (num);
+}
+
+
+/**
+ * init_lineptr - helper function
+ * @lineptr: pointer to memory
+ * @n: line buffer size
+ *
+ */
+
+void init_lineptr(char **lineptr, size_t *n)
+{
+	*n = BUFFER_SIZE;
+	*lineptr = malloc(*n);
+	if (*lineptr == NULL)
+	{
+		perror("Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+/**
+ * fill_buff - helper function to fill buff
+ * @buff: pointer to memory
+ * @idx: buffer index
+ * @B_read: bytes read
+ *
+ */
+
+void fill_buff(char *buff, size_t *idx, size_t *B_read)
+{
+	*B_read = read(STDIN_FILENO, buff, BUFFER_SIZE);
+	if (*B_read <= 0)
+	{
+		if (*idx == 0)
+			exit(-1);
+	}
+	*idx = 0;
+}
+
+
+/**
+ * handle_chare - helper function to handle characters
+ * @c: character to handle
+ * @lineptr: pointer to memory
+ * @n: line buffer size
+ * @num: integer
+ *
+ * Return: 0 on success
+ */
+
+int handle_char(char c, char **lineptr, size_t *n, size_t *num)
+{
+	if (c == '\n')
+	{
+		(*lineptr)[*num] = '\0'; /* Null-terminate the string */
+		return (1);
+	}
+
+	if (*num + 1 >= *n)
+		realloc_lineptr(lineptr, n);
+
+	(*lineptr)[*num++] = c;
+	return (0);
+}
+
+
+/**
+ * realloc_lineptr  - helper function to reallocate lineptr
+ * @lineptr: pointer to memory
+ * @n: line buffer size
+ *
+ */
+
+void realloc_lineptr(char **lineptr, size_t *n)
+{
+	*n += BUFFER_SIZE;
+	*lineptr = _realloc(*lineptr, *n - BUFFER_SIZE, *n);
+	if (*lineptr == NULL)
+	{
+		perror("Memory reallocation failed");
+		exit(EXIT_FAILURE);
+	}
 }
 
--- shell_main.h
+++ shell_main.h
@@ -51,7 +51,10 @@ int _strncmp(const char *str1, const char *str2, size_t n);
 /* Prototypes for helping functions */
 void cleanup(char **cmd, int count);
 int _delim(char c, const char *delim);
-
+void init_lineptr(char **lineptr, size_t *n);
+void fill_buff(char *buff, size_t *idx, size_t *B_read);
+int handle_char(char c, char **lineptr, size_t *n, size_t *num);
+void realloc_lineptr(char **lineptr, size_t *n);
 
 
 /* Function prototypes */
